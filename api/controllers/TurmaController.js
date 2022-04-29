56064eb1d008ab4f08f429630282e92c31e3e62d
izabela-dsn <izabeladasilva10@gmail.com>
2022-04-29 14:45:57 -0300

    filtro de data

--- api/controllers/TurmaController.js
+++ api/controllers/TurmaController.js
@@ -1,9 +1,16 @@
 const database = require("../models")
+const Sequelize = require("sequelize")
+const op = Sequelize.Op
 
 class TurmaController {
   static async pegaTodasAsTurmas(req, res) {
+    const { data_inicial, data_final } = req.query
+    const where = {}
+    data_inicial || data_final ? (where.data_incio = {}) : null
+    data_inicial ? (where.data_incio[op.gte] = data_inicial) : null
+    data_final ? (where.data_incio[op.lte] = data_final) : null
     try {
-      const todasAsTurmas = await database.Turmas.findAll()
+      const todasAsTurmas = await database.Turmas.findAll({ where })
       return res.status(200).json(todasAsTurmas)
     } catch (error) {
       return res.status(500).json(error.message)
