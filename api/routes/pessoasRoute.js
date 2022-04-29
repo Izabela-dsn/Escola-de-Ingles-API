5e333998edc2d82cf6b63ce6bf624b57bce9e03c
izabela-dsn <izabeladasilva10@gmail.com>
2022-04-29 15:32:49 -0300

    verificar lotação de turmas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,45 +3,39 @@ const PessoaController = require("../controllers/PessoaController")
 
 const router = Router()
 
-// rota/endpoint para pegar todas a pessoas
 router.get("/pessoas/all", PessoaController.pegaTodasAsPessoas)
-// rota/endpoint para pegar todas a pessoas ativas
 router.get("/pessoas", PessoaController.pegaTodasAsPessoasAtivas)
-// endpoint para pegar uma pessoa /pessoas/:id é o parametro passado
 router.get("/pessoas/:id", PessoaController.pegaUmaPessoa)
-
-router.get("/pessoas/:estudanteId/matricula", PessoaController.pegaMatriculas)
 router.get(
   "/pessoas/matricula/:turmaId/confirmadas",
   PessoaController.pegaMatriculasPorTurma
 )
-
-// endpoint para criar
-router.post("/pessoas", PessoaController.criaPessoa)
-// endpoint para editar registros
-router.put("/pessoas/:id", PessoaController.atualizaPessoa)
-// endpoint para deletar registros
-router.delete("/pessoas/:id", PessoaController.apagaPessoa)
-router.post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
-
 router.get(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.pegaUmaMatricula
 )
+router.get("/pessoas/:estudanteId/matricula", PessoaController.pegaMatriculas)
+router.get("/pessoas/matricula/lotada", PessoaController.pegaTurmasLotadas)
+
+router.post("/pessoas", PessoaController.criaPessoa)
+router.post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
 router.post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula)
+router.post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
+router.post(
+  "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
+  PessoaController.restauraMatricula
+)
+
+router.put("/pessoas/:id", PessoaController.atualizaPessoa)
 router.put(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.atualizaMatricula
 )
+
+router.delete("/pessoas/:id", PessoaController.apagaPessoa)
 router.delete(
   "/pessoas/:estudanteId/matricula/:matriculaId",
   PessoaController.apagaMatricula
-)
-router.post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
-
-router.post(
-  "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
-  PessoaController.restauraMatricula
 )
 
 module.exports = router
